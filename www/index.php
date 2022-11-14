1e8d28d912a7941137aa2dba8d55c3d8a7cebf8d
IlyaSushkevich <ilya.sushkevich@gmail.com>
2022-11-14 14:18:07 +0300

    Работа с роутингом

--- www/index.php
+++ www/index.php
@@ -4,4 +4,24 @@ spl_autoload_register(function (string $className) {
     require_once '../src/' . str_replace('\\', '/', $className) . '.php';
 });
 
-var_dump($_GET['route']);
+$route = $_GET['route'] ?? '';
+
+$pattern = '~^hello/(.*)$~';
+preg_match($pattern, $route, $matches);
+
+if (!empty($matches)) {
+    $controller = new \MyProject\Controllers\MainController();
+    $controller->sayHello($matches[1]);
+    return;
+}
+
+$pattern = '~^$~';
+preg_match($pattern, $route, $matches);
+
+if (!empty($matches)) {
+    $controller = new \MyProject\Controllers\MainController();
+    $controller->main();
+    return;
+}
+
+echo 'Страница не найдена';

40ce13902c8e94729aa5f336fd05b110a800845b
IlyaSushkevich <ilya.sushkevich@gmail.com>
2022-11-16 12:25:47 +0300

    Вывод CSS в отдельный файл

--- templates/main/main.php
+++ templates/main/main.php
@@ -3,41 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <title>Мой блог</title>
-    <style>
-        .layout {
-            width: 100%;
-            max-width: 1024px;
-            margin: auto;
-            background-color: white;
-            border-collapse: collapse;
-        }
-
-        .layout tr td {
-            padding: 20px;
-            vertical-align: top;
-            border: solid 1px gray;
-        }
-
-        .header {
-            font-size: 30px;
-        }
-
-        .footer {
-            text-align: center;
-        }
-
-        .sidebarHeader {
-            font-size: 20px;
-        }
-
-        .sidebar ul {
-            padding-left: 20px;
-        }
-
-        a, a:visited {
-            color: darkgreen;
-        }
-    </style>
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
 

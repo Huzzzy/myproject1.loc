c745faa34c0005c7c51e621cf885f2d5809d556a
IlyaSushkevich <ilya.sushkevich@gmail.com>
2022-11-16 20:26:36 +0300

    Передача статей в шаблон

--- src/MyProject/Controllers/MainController.php
+++ src/MyProject/Controllers/MainController.php
@@ -3,23 +3,26 @@
 namespace MyProject\Controllers;
 
 use MyProject\View\View;
+use MyProject\Services\Db;
 
 class MainController
 {
+    /** @var View */
     private $view;
+
+    /** @var Db */
+    private $db;
 
     public function __construct()
     {
         $this->view = new View(__DIR__ . '/../../../templates');
+        $this->db = new Db();
     }
 
     public function main()
     {
-        $articles = [
-            ['name' => 'Статья 1', 'text' => 'Текст статьи 1'],
-            ['name' => 'Статья 2', 'text' => 'Текст статьи 2'],
-        ];
         $title = '';
+        $articles = $this->db->query('SELECT * FROM `articles`;');
         $this->view->renderHtml('main/main.php', ['articles' => $articles], $title);
     }
     public function sayHello(string $name)

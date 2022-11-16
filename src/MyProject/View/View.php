21e9135954cba4d5d75d389be26c7dab8e985bd7
IlyaSushkevich <ilya.sushkevich@gmail.com>
2022-11-16 13:24:33 +0300

    Добавления буфера вывода

--- src/MyProject/View/View.php
+++ src/MyProject/View/View.php
@@ -10,11 +10,16 @@ class View
     {
         $this->templatesPath = $templatesPath;
     }
-    
+
     public function renderHtml(string $templateName, array $vars = [])
     {
         extract($vars);
 
+        ob_start();
         include $this->templatesPath . '/' . $templateName;
+        $buffer = ob_get_contents();
+        ob_end_clean();
+
+        echo $buffer;
     }
 }

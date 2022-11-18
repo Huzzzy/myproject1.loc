187bb2f4b884f5af23ce330c260b5c646b96b61f
IlyaSushkevich <ilya.sushkevich@gmail.com>
2022-11-18 12:06:05 +0300

    Добавление роута для изменения статей

--- src/routes.php
+++ src/routes.php
@@ -3,6 +3,7 @@
 return [
     '~^hello/(.*)$~' => [\MyProject\Controllers\MainController::class, 'sayHello'],
     '~^articles/(\d+)$~' => [\MyProject\Controllers\ArticlesController::class, 'view'],
+    '~^articles/(\d+)/edit$~' => [\MyProject\Controllers\ArticlesController::class, 'edit'],
     '~^$~' => [\MyProject\Controllers\MainController::class, 'main'],
     '~^bye/(.*)$~' => [\MyProject\Controllers\MainController::class, 'sayBye']
 ];

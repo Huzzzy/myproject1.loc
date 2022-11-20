244cb9c3c6a761f21c49a3fd10a332987fa9fe77
IlyaSushkevich <ilya.sushkevich@gmail.com>
2022-11-20 11:35:40 +0300

    "Поломка" путя к БД, Создание класса для обработки исключений

--- src/settings.php
+++ src/settings.php
@@ -3,7 +3,7 @@
 return [
     'db' => [
         'host' => 'localhost',
-        'dbname' => 'my_project',
+        'dbname' => 'my_project2',
         'user' => 'root',
         'password' => '',
     ]

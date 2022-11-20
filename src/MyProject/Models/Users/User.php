dff9dd6e3208edeecb5b16f3e630d344d733b5e2
IlyaSushkevich <ilya.sushkevich@gmail.com>
2022-11-20 13:56:25 +0300

    Проверку на то, что длина пароля не менее восьми символов, а также что nickname содержит только допустимые символы, а email является корректным email-ом

--- src/MyProject/Models/Users/User.php
+++ src/MyProject/Models/Users/User.php
@@ -46,13 +46,23 @@ class User extends ActiveRecordEntity
         if (empty($userData['nickname'])) {
             throw new InvalidArgumentException('Не передан nickname');
         }
+        if (!preg_match('/^[a-zA-Z0-9]+$/', $userData['nickname'])) {
+            throw new InvalidArgumentException('Nickname может состоять только из символов латинского алфавита и цифр');
+        }
+    
     
         if (empty($userData['email'])) {
             throw new InvalidArgumentException('Не передан email');
         }
+        if (!filter_var($userData['email'], FILTER_VALIDATE_EMAIL)) {
+            throw new InvalidArgumentException('Email некорректен');
+        }
     
         if (empty($userData['password'])) {
             throw new InvalidArgumentException('Не передан password');
+        }
+        if (mb_strlen($userData['password']) < 8) {
+            throw new InvalidArgumentException('Пароль должен быть не менее 8 символов');
         }
     }
 }

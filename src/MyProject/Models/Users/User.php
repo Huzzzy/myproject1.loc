7403b1bbfed0954f3d94d04f1b2d09865bec6b2d
IlyaSushkevich <ilya.sushkevich@gmail.com>
2022-11-20 14:07:29 +0300

    Добавление проверки, что пользователя с такими email и nickname нет в базе

--- src/MyProject/Models/Users/User.php
+++ src/MyProject/Models/Users/User.php
@@ -43,21 +43,30 @@ class User extends ActiveRecordEntity
 
     public static function signUp(array $userData)
     {
+        //NICKNAME
         if (empty($userData['nickname'])) {
             throw new InvalidArgumentException('Не передан nickname');
         }
         if (!preg_match('/^[a-zA-Z0-9]+$/', $userData['nickname'])) {
             throw new InvalidArgumentException('Nickname может состоять только из символов латинского алфавита и цифр');
         }
+        if (static::findOneByColumn('nickname', $userData['nickname']) !== null) {
+            throw new InvalidArgumentException('Пользователь с таким nickname уже существует');
+        }
     
-    
+        
+        //EMAIL
         if (empty($userData['email'])) {
             throw new InvalidArgumentException('Не передан email');
         }
         if (!filter_var($userData['email'], FILTER_VALIDATE_EMAIL)) {
             throw new InvalidArgumentException('Email некорректен');
         }
-    
+        if (static::findOneByColumn('email', $userData['email']) !== null) {
+            throw new InvalidArgumentException('Пользователь с таким email уже существует');
+        }
+        
+        //PASSWORD
         if (empty($userData['password'])) {
             throw new InvalidArgumentException('Не передан password');
         }

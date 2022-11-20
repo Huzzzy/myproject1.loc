37a242c70d98ec938a5063d2585e7531e0e7449b
IlyaSushkevich <ilya.sushkevich@gmail.com>
2022-11-20 12:31:46 +0300

    Добавление роута для регистрации

--- src/routes.php
+++ src/routes.php
@@ -6,6 +6,7 @@ return [
     '~^articles/(\d+)/edit$~' => [\MyProject\Controllers\ArticlesController::class, 'edit'],
     '~^articles/add$~' => [\MyProject\Controllers\ArticlesController::class, 'add'],
     '~^articles/(\d+)/delete$~' => [\MyProject\Controllers\ArticlesController::class, 'delete'],
+    '~^users/register$~' => [\MyProject\Controllers\UsersController::class, 'signUp'],
     '~^$~' => [\MyProject\Controllers\MainController::class, 'main'],
     '~^bye/(.*)$~' => [\MyProject\Controllers\MainController::class, 'sayBye']
 ];

7922f2f268aaa1e9af1c76907ad5bab1393bd85b
IlyaSushkevich <ilya.sushkevich@gmail.com>
2022-11-20 11:52:25 +0300

    Обработка ошибки о том, что страница не найдена

--- www/index.php
+++ www/index.php
@@ -18,8 +18,7 @@ try {
     }
 
     if (!$isRouteFound) {
-        echo 'Страница не найдена!';
-        return;
+        throw new \MyProject\Exceptions\NotFoundException();
     }
 
     unset($matches[0]);
@@ -32,4 +31,7 @@ try {
 } catch (\MyProject\Exceptions\DbException $e) {
     $view = new \MyProject\View\View(__DIR__ . '/../templates/errors');
     $view->renderHtml('500.php', ['error' => $e->getMessage()], 500);
+} catch (\MyProject\Exceptions\NotFoundException $e) {
+    $view = new \MyProject\View\View(__DIR__ . '/../templates/errors');
+    $view->renderHtml('404.php', ['error' => $e->getMessage()], 404);
 }

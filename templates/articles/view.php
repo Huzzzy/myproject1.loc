a3d6c2f7157ef0c6d903f5918d427666648ff0b6
IlyaSushkevich <ilya.sushkevich@gmail.com>
2022-11-22 14:44:47 +0300

    Добавление в шаблоне ссылки для редактирования статьи, которая отображается пользователям с правами администратора

--- templates/articles/view.php
+++ templates/articles/view.php
@@ -2,4 +2,7 @@
     <h1><?= $article->getName() ?></h1>
     <p><?= $article->getText() ?></p>
     <p>Автор: <?= $article->getAuthor()->getNickname() ?></p>
+    <?php if(!empty($user) && $user->getRole() === 'admin'): ?>
+    <a href="http://myproject1.loc/articles/<?= $article->getId()?>/edit">Редактировать</a>
+    <? endif; ?>
 <?php include __DIR__ . '/../footer.php'; ?>

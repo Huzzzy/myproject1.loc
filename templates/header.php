8aa9ebbaa4010bd1451e62f59a7d57a80ac28934
IlyaSushkevich <ilya.sushkevich@gmail.com>
2022-11-24 12:02:53 +0300

    Ссылка на добавление статьи

--- templates/header.php
+++ templates/header.php
@@ -14,12 +14,18 @@
         </td>
     </tr>
     <tr>
-        <td colspan="2" style="text-align: right">
-            <?php if(!empty($user)): ?>
-                Привет, <?= $user->getNickname() ?>  | <a href="http://myproject1.loc/users/logout">Выйти</a>
-            <?php else: ?>
-                <a href="http://myproject1.loc/users/login">Войти</a> | <a href="http://myproject1.loc/users/register">Зарегестрироваться</a>
+        <td colspan="2">
+            <?php if (!empty($user) && $user->getRole() === 'admin'): ?>
+                <a href="http://myproject1.loc/articles/add">Добавить статью</a>
             <? endif; ?>
+            <div style="text-align: right">
+                <?php if (!empty($user)): ?>
+                    Привет, <?= $user->getNickname() ?>  | <a href="http://myproject1.loc/users/logout">Выйти</a>
+                <?php else: ?>
+                    <a href="http://myproject1.loc/users/login">Войти</a> | <a
+                            href="http://myproject1.loc/users/register">Зарегестрироваться</a>
+                <? endif; ?>
+            </div>
         </td>
     </tr>
     <tr>

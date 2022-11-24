399e22064ebd8b9f873501a94da7e56b35933931
IlyaSushkevich <ilya.sushkevich@gmail.com>
2022-11-24 14:06:02 +0300

    Форма добавления комментариев

--- templates/articles/view.php
+++ templates/articles/view.php
@@ -2,8 +2,18 @@
     <h1><?= $article->getName() ?></h1>
     <p><?= $article->getText() ?></p>
     <p>Автор: <?= $article->getAuthor()->getNickname() ?></p>
-    <?php if(!empty($user) && $user->getRole() === 'admin'): ?>
-    <a href="http://myproject1.loc/articles/<?= $article->getId()?>/edit">Редактировать</a>
-    <a href="http://myproject1.loc/articles/<?= $article->getId()?>/delete">Удалить</a>
-    <? endif; ?>
+    <h1>Комментарии</h1>
+    <p><?= $comments->getText() ?></p>
+    <?php if(!empty($user)){ ?>
+    <form action="/articles/<?= $article->getId()?>/comments" method="post">
+    <label for="text">Написать комментарий</label><br>
+    <textarea name="text" id="text" rows="4" cols="50"><?= $_POST['text'] ?? '' ?></textarea><br>
+    <br>
+    <input type="submit" value="Опубликовать">
+    </form>
+    <?php } ?>
+    <?php if(!empty($user) && $user->getRole() === 'admin'){ ?>
+    <a href="/articles/<?= $article->getId()?>/edit">Редактировать</a>
+    <a href="/articles/<?= $article->getId()?>/delete">Удалить</a>
+    <?php } ?>
 <?php include __DIR__ . '/../footer.php'; ?>

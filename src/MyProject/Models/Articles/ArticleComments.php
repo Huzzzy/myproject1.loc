58594364378594942a174126942b12d75135d501
IlyaSushkevich <ilya.sushkevich@gmail.com>
2022-11-24 14:24:45 +0300

    Метод для записи данных комментария

---
+++ src/MyProject/Models/Articles/ArticleComments.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace MyProject\Models\Articles;
+
+use MyProject\Exceptions\InvalidArgumentException;
+use MyProject\Models\ActiveRecordEntity;
+use MyProject\Models\Users\User;
+use MyProject\Models\Articles\Article;
+
+class ArticleComments extends ActiveRecordEntity
+{
+    /** @var string */
+    protected $articleId;
+
+    /** @var string */
+    protected $authorId;
+
+    /** @var string */
+    protected $text;
+
+    /** @var string */
+    protected $createdAt;
+
+    protected static function getTableName(): string
+    {
+        return 'comments';
+    }
+
+    /**
+     * @return int
+     */
+
+    public function setArticleId(Article $id):void
+    {
+        $this->articleId = $id->getId();
+    }
+
+    public function setText($text):void
+    {
+        $this->text = $text->getId();
+    }
+
+    public function setAuthor(User $author):void
+    {
+        $this->authorId = $author->getId();
+    }
+
+    public static function createFromArray(array $fields, User $author):ArticleComments
+    {
+        if (empty($fields['text'])) {
+            throw new InvalidArgumentException('Не передан текст комментария');
+        }
+
+        $comment = new ArticleComments();
+
+        $comment->setAuthor($author);
+        $comment->setText($fields['text']);
+
+        $comment->save();
+
+        return $comment;
+    }
+
+}

112c05984175b99a8724a0ee3fc400ffea5574ec
IlyaSushkevich <ilya.sushkevich@gmail.com>
2022-11-28 13:57:52 +0300

    Укорачивание текста

--- templates/admin/admin.php
+++ templates/admin/admin.php
@@ -2,7 +2,7 @@
 <h2>Последние статьи</h2>
 <?php foreach ($articles as $article){ ?>
     <h2><a href="/articles/<?= $article->getId() ?>"><?= $article->getName() ?></a></h2>
-    <p><?= $article->getText() ?></p>
+    <p><?= $article->getShortText() ?></p>
     <hr>
 <?php } ?>
 <h2>Последние комментарии</h2>

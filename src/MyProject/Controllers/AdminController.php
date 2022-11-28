1f8e510409a2a003ac1f5da89dcbbecbce7d4aba
IlyaSushkevich <ilya.sushkevich@gmail.com>
2022-11-28 14:05:29 +0300

    Условия для входа на страницу

--- src/MyProject/Controllers/AdminController.php
+++ src/MyProject/Controllers/AdminController.php
@@ -3,6 +3,8 @@
 namespace MyProject\Controllers;
 
 use MyProject\Controllers\AbstractController;
+use MyProject\Exceptions\ForbiddenException;
+use MyProject\Exceptions\UnauthorizedException;
 use MyProject\Models\Articles\Article;
 use MyProject\Models\Articles\ArticleComments;
 
@@ -10,6 +12,14 @@ use MyProject\Models\Articles\ArticleComments;
 class AdminController extends AbstractController {
     public function admin()
     {
+        if ($this->user === null) {
+            throw new UnauthorizedException();
+        }
+
+        if ($this->user->getRole() !== 'admin') {
+                throw new ForbiddenException();
+        }
+
         $articles = Article::findTheLastThreeColumns();
         $comments = ArticleComments::findTheLastThreeColumns();
         $this->view->renderHtml('admin/admin.php', [

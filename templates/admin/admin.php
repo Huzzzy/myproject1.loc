29960f4f0c469d3342a10ee7bb50383761e973a2
IlyaSushkevich <ilya.sushkevich@gmail.com>
2022-11-28 13:42:22 +0300

    Отображение в шаблоне последних статей и последних комментариев

--- templates/admin/admin.php
+++ templates/admin/admin.php
@@ -1,6 +1,13 @@
 <?php include __DIR__ . '/../header.php'; ?>
 <h2>Последние статьи</h2>
-
+<?php foreach ($articles as $article){ ?>
+    <h2><a href="/articles/<?= $article->getId() ?>"><?= $article->getName() ?></a></h2>
+    <p><?= $article->getText() ?></p>
+    <hr>
+<?php } ?>
 <h2>Последние комментарии</h2>
-
+<?php foreach ($comments as $comment){ ?>
+    <p id="comment<?php $comment->getId() ?>"><?= $comment->getAuthor()->getNickname() ?>
+        <?= $comment->getText() ?></p>
+<?php } ?>
 <?php include __DIR__ . '/../footer.php'; ?>

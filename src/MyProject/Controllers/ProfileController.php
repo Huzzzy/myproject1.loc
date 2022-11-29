01a178d1011cb848504782f852bab50d21c68035
IlyaSushkevich <ilya.sushkevich@gmail.com>
2022-11-29 14:01:18 +0300

    Добавление формы загрузки фото

--- src/MyProject/Controllers/ProfileController.php
+++ src/MyProject/Controllers/ProfileController.php
@@ -2,10 +2,17 @@
 
 namespace MyProject\Controllers;
 
+use MyProject\Models\Users\User;
+
 class ProfileController extends AbstractController
 {
-    public function profile()
+    public function profile():void
     {
-        $this->view->renderHtml('users/profile.php', []);
+
+
+        $this->view->renderHtml('users/profile.php',
+            [
+
+            ]);
     }
 }
--- templates/users/profile.php
+++ templates/users/profile.php
@@ -1,3 +1,11 @@
 <?php include __DIR__ . '/../header.php'; ?>
+    <div>
+        <?= $user->getNickname() ?>
+    </div>
 
+    <h2>Загрузить фотографию</h2>
+<form action="/" method="post" enctype="multipart/form-data">
+    <input type="file" name="attachment">
+    <input type="submit">
+</form>
 <?php include __DIR__ . '/../footer.php'; ?>

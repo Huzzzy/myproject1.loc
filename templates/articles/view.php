1c10f2ef1cf57501455cb7f76ff3849cfa2a900d
IlyaSushkevich <ilya.sushkevich@gmail.com>
2022-11-29 15:02:23 +0300

    Вывод прошедшего парсинг текста в шаблоне

--- templates/articles/view.php
+++ templates/articles/view.php
@@ -1,6 +1,6 @@
 <?php include __DIR__ . '/../header.php'; ?>
     <h1><?= $article->getName() ?></h1>
-    <p><?= $article->getText() ?></p>
+    <p><?= $article->getParsedText() ?></p>
     <p>Автор: <?= $article->getAuthor()->getNickname() ?></p>
 
     <h1>Комментарии</h1>

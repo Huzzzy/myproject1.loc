f8f24355a24376bd8e629a4b409048532a969001
IlyaSushkevich <ilya.sushkevich@gmail.com>
2022-12-02 13:59:02 +0300

    Реализация в шаблоне пагинации: "туда-сюда"

--- templates/main/main.php
+++ templates/main/main.php
@@ -6,12 +6,16 @@
 <?php endforeach; ?>
 
     <div style="text-align: center">
-        <?php for ($pageNum = 1; $pageNum <= $pagesCount; $pageNum++): ?>
-            <?php if ($currentPageNum === $pageNum): ?>
-                <b><?= $pageNum ?></b>
-            <?php else: ?>
-                <a href="/<?= $pageNum === 1 ? '' : $pageNum ?>"><?= $pageNum ?></a>
-            <?php endif; ?>
-        <?php endfor; ?>
+        <?php if ($previousPageLink !== null): ?>
+            <a href="<?= $previousPageLink ?>">&lt; Туда</a>
+        <?php else: ?>
+            <span style="color: grey">&lt; Туда</span>
+        <?php endif; ?>
+        &nbsp;&nbsp;&nbsp;
+        <?php if ($nextPageLink !== null): ?>
+            <a href="<?= $nextPageLink ?>">Сюда &gt;</a>
+        <?php else: ?>
+            <span style="color: grey">Сюда &gt;</span>
+        <?php endif; ?>
     </div>
 <?php include __DIR__ . '/../footer.php'; ?>

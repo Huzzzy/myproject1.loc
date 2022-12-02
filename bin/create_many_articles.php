fbbf6f8de7bbee862048c68e88b62306aa80054b
IlyaSushkevich <ilya.sushkevich@gmail.com>
2022-12-02 13:47:18 +0300

    Исправление пути

--- bin/create_many_articles.php
+++ bin/create_many_articles.php
@@ -1,7 +1,7 @@
 <?php
 
 spl_autoload_register(function (string $className) {
-    require_once __DIR__ . '/src/' . str_replace('\\', '/', $className) . '.php';
+    require_once __DIR__ . '/../src/' . str_replace('\\', '/', $className) . '.php';
 });
 
 $db = \MyProject\Services\Db::getInstance();

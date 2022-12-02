29bef80302768c56f8d3bb375a4f4df0148d973f
IlyaSushkevich <ilya.sushkevich@gmail.com>
2022-12-02 14:17:32 +0300

    Реализация отображения страниц до и после их id. Реализация отображения главной страницы

--- src/MyProject/Controllers/MainController.php
+++ src/MyProject/Controllers/MainController.php
@@ -9,30 +9,36 @@ class MainController extends AbstractController
 {
     public function main()
     {
-        $this->page(1);
-    }
-
-    public function page(int $pageNum)
-    {
-        $pagesCount = Article::getPagesCount(5);
-        $this->view->renderHtml('main/main.php', [
-            'articles' => Article::getPage($pageNum, 5),
-            'previousPageLink' => $pageNum > 1
-                ? '/' . ($pageNum - 1)
-                : null,
-            'nextPageLink' => $pageNum < $pagesCount
-                ? '/' . ($pageNum + 1)
-                : null
-        ]);
+        $lastID = Article::getLastID();
+        if ($lastID === null) {
+            throw new NotFoundException();
+        }
+        $this->after($lastID + 1);
     }
 
     public function before(int $id)
     {
-
+        $this->page(Article::getPageBefore($id, 5));
     }
 
     public function after(int $id)
     {
+        $this->page(Article::getPageAfter($id, 5));
+    }
 
+    private function page(array $articles)
+    {
+        if ($articles === []) {
+            throw new NotFoundException();
+        }
+
+        $firstID = $articles[0]->getId();
+        $lastID = $articles[count($articles)-1]->getId();
+
+        $this->view->renderHtml('main/main.php', [
+            'articles' => $articles,
+            'previousPageLink' => Article::hasPreviousPage($firstID) ? '/before/' . $firstID : null,
+            'nextPageLink' => Article::hasNextPage($lastID) ? '/after/' . $lastID : null,
+        ]);
     }
 }
